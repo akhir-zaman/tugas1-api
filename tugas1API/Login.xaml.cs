fc029b6e8217a53a2f600974faee16312e041662
Faldiansyah <faldiansyahk@gmail.com>
2023-05-30 20:20:19 +0800

    first commit

---
+++ tugas1API/Login.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace tugas1API
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public static string loginEmail;
+        public static string loginPass;
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void lblDaftar_Click(object sender, RoutedEventArgs e)
+        {
+            regis register = new regis();
+            register.Show();
+
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            loginEmail = logEmail.Text;
+            loginPass = logPass.Password;
+
+            if (loginEmail != regis.email)
+            {
+                MessageBox.Show("Email Salah");
+                return;
+            }
+            if(loginPass != regis.password)
+            {
+                MessageBox.Show("Password salah");
+                return;
+            }
+            MainWindow main = new MainWindow();
+            main.Show();
+        }
+    }
+}
---
+++ tugas1API/regis.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace tugas1API
+{
+    /// <summary>
+    /// Interaction logic for regis.xaml
+    /// </summary>
+    public partial class regis : Window
+    {
+        public static string namaDepan;
+        public static string namaBelakang;
+        public static string email;
+        public static string password;
+        public static string konfirm;
+
+        public regis()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            namaDepan = txtNamaDepan.Text;
+            namaBelakang = txtNamaBelakang.Text;
+            email = txtEmail.Text;
+            password = pass1.Password;
+            konfirm = konfpass.Password;
+            if (email == "" && namaBelakang == "" && namaDepan == "" && password == "" && konfirm == "")
+            {
+                MessageBox.Show("Field tidak boleh kosong!");
+            }
+            else
+            {
+
+                if (password != konfirm)
+                {
+                    MessageBox.Show("Password tidak sesuai");
+                    return;
+                }
+                else
+                {
+                    Close();
+                    MessageBox.Show("Registrasi berhasil");
+                }
+            }
+            
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            txtNamaDepan.Text = "";
+            txtNamaBelakang.Text = "";
+            txtEmail.Text = "";
+            pass1.Password = "";
+            konfpass.Password = "";
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
